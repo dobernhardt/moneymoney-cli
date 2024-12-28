93e1a649d1690f808acb6e172f2112e309f1cdc6
Dominik Bernhardt <dominik@dokabe.de>
2024-12-28 13:15:21 +0100

    chore: replaced pretty table with rich and refactored files

--- moneymoney_cli/__main__.py
+++ moneymoney_cli/__main__.py
@@ -1,15 +1,9 @@
 import datetime
 
-from .moneymoneydb import MoneyMoneyDB
-
 import click
-import gensim.downloader as api
-import joblib
-import numpy as np
-import pandas as pd
-import prettytable
 
-from sklearn.base import BaseEstimator, TransformerMixin
+import joblib
+import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
@@ -17,22 +11,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 
-
-class RelativeDate(click.ParamType):
-    name = "relative_date"
-
-    def convert(self, value, param, ctx):
-        try:
-            if value.endswith("Y"):
-                return datetime.datetime.now() - datetime.timedelta(days=int(value[:-1]) * 365)
-            elif value.endswith("M"):
-                return datetime.datetime.now() - datetime.timedelta(days=int(value[:-1]) * 30)
-            elif value.endswith("D"):
-                return datetime.datetime.now() - datetime.timedelta(days=int(value[:-1]))
-            else:
-                return datetime.datetime.strptime(value, "%Y-%m-%d")
-        except ValueError:
-            self.fail(f"{value} is not a valid date", param, ctx)
+from .moneymoneydb import MoneyMoneyDB
+from .relative_date import RelativeDate
+from .word_vec import Word2VecVectorizer
+from rich.console import Console
+from rich.table import Table
 
 
 @click.group()
@@ -44,33 +27,25 @@ def cli():
 @click.option("--db-password", help="Encryption password of moneymoney DB", required=True)
 def list_accounts(db_password):
     db = MoneyMoneyDB(db_password)
-    table = prettytable.PrettyTable()
-    table.field_names = ["ID", "Account name"]
-    table.align = "l"
+    console = Console()
+    table = Table(title="Accounts")
+
+    table.add_column("ID", justify="left", style="cyan", no_wrap=True)
+    table.add_column("Account name", justify="left", style="magenta")
+
     for account in db.get_accounts():
-        table.add_row(account)
-    print(table)
+        table.add_row(str(account[0]), account[1])
+
+    console.print(table)
 
 
 @cli.command()
+# fmt: off
 @click.option("--db-password", help="Encryption password of moneymoney DB", required=True)
-@click.option(
-    "--date-from",
-    type=RelativeDate(),
-    default="2Y",
-    help="Oldest transaction to be categorized (e.g., 1Y for one year ago or 2021-01-01 for an absolute date)",
-    required=True,
-    show_default=True,
-)
-@click.option(
-    "--date-to",
-    type=RelativeDate(),
-    default=(datetime.datetime.now() + datetime.timedelta(days=30)).strftime("%Y-%m-%d"),
-    help="Newest transaction to be categorized",
-    required=True,
-    show_default=True,
-)
+@click.option("--date-from", type=RelativeDate(), default="2Y", help="Oldest transaction to be categorized (e.g., 1Y for one year ago or 2021-01-01 for an absolute date)", required=True, show_default=True)  # noqa: E501
+@click.option("--date-to", type=RelativeDate(), default=(datetime.datetime.now() + datetime.timedelta(days=30)).strftime("%Y-%m-%d"), help="Newest transaction to be categorized", required=True, show_default=True)  # noqa: E501
 @click.option("--limit-to-account", help="Limit classification to transactions in the defined account. Can be provided multiple times", multiple=True)
+# fmt: on
 def list_category_usage(db_password, date_from, date_to, limit_to_account):
     """
     List the usage of categories in the transactions.
@@ -85,44 +60,33 @@ def list_category_usage(db_password, date_from, date_to, limit_to_account):
     """
     db = MoneyMoneyDB(db_password)
     category_usage = db.get_category_usage(date_from, date_to, limit_to_account)
-    table = prettytable.PrettyTable()
     categories = db.get_categories()
-    table.field_names = ["Category ID", "Category name", "# of transactions"]
-    table.align = "l"
+
+    console = Console()
+    table = Table(title="Category Usage")
+
+    table.add_column("Category ID", justify="left", style="cyan", no_wrap=True)
+    table.add_column("Category name", justify="left", style="magenta")
+    table.add_column("# of transactions", justify="left", style="green")
+
     for category_id, usage_count in category_usage.items():
-        table.add_row([category_id, categories[category_id], usage_count])
-    print(table)
+        table.add_row(str(category_id), categories[category_id], str(usage_count))
+
+    console.print(table)
 
 
 @cli.command()
+# fmt: off
 @click.option("--db-password", help="Encryption password of moneymoney DB", required=True)
-@click.option(
-    "--date-from",
-    type=RelativeDate(),
-    default="3M",
-    help="Oldest transaction to be categorized (e.g., 1Y for one year ago or 2021-01-01 for an absolute date)",
-    required=True,
-    show_default=True,
-)
-@click.option(
-    "--date-to",
-    type=RelativeDate(),
-    default=(datetime.datetime.now() + datetime.timedelta(days=30)).strftime("%Y-%m-%d"),
-    help="Newest transaction to be categorized",
-    required=True,
-    show_default=True,
-)
+@click.option("--date-from", type=RelativeDate(), default="3M", help="Oldest transaction to be categorized (e.g., 1Y for one year ago or 2021-01-01 for an absolute date)", required=True, show_default=True)  # noqa: E501
+@click.option("--date-to", type=RelativeDate(), default=(datetime.datetime.now() + datetime.timedelta(days=30)).strftime("%Y-%m-%d"), help="Newest transaction to be categorized", required=True, show_default=True)  # noqa: E501
 @click.option("--limit-to-account", help="Limit classification to transactions in the defined account. Can be provided multiple times", multiple=True)
 @click.option("--model-name", help="Specify the model to be used", required=True, default="default", show_default=True)
-@click.option(
-    "--propability-threshold",
-    help="Specify the prediction threshold. Only predictions with a higher propability are conciders",
-    required=True,
-    default=0.75,
-    show_default=True,
-)
+@click.option("--propability-threshold", help="Specify the prediction threshold. Only predictions with a higher propability are conciders", required=True, default=0.75, show_default=True)   # noqa: E501
 @click.option("--apply", help="Apply the categorization to the database", is_flag=True)
-def categorize(db_password, date_from, date_to, limit_to_account, model_name, apply, propability_threshold):
+@click.option("--overwrite", help="Categorize also already categorized transactions", is_flag=True)
+# fmt: on
+def categorize(db_password, date_from, date_to, limit_to_account, model_name, apply, propability_threshold, overwrite):
     """
     Categorize transactions using a trained machine learning model.
 
@@ -138,7 +102,7 @@ def categorize(db_password, date_from, date_to, limit_to_account, model_name, ap
     and optionally updates the database with the predicted categories if the --apply flag is specified.
     """
     db = MoneyMoneyDB(db_password)
-    transactions = list(db.get_transactions(date_from, date_to, limit_to_account, only_uncategorized=True))
+    transactions = list(db.get_transactions(date_from, date_to, limit_to_account, only_uncategorized=(not overwrite)))
 
     # Create a DataFrame from the transactions
     df = pd.DataFrame([t.__dict__ for t in transactions])
@@ -167,64 +131,45 @@ def categorize(db_password, date_from, date_to, limit_to_account, model_name, ap
     df["probability"] = predicted_probabilities
 
     # Print the categorized transactions with probabilities
-    print(df[["transaction_id", "purpose", "name", "amount", "predicted_category", "probability"]].to_string(index=False))
+    console = Console()
+    table = Table(title="Categorized Transactions")
+
+    table.add_column("Purpose", justify="left")
+    table.add_column("Name", justify="left")
+    table.add_column("Amount", justify="right")
+    table.add_column("Predicted Category", justify="left")
+    table.add_column("Probability", justify="right")
+
+    for _, row in df.iterrows():
+        probability_style = "bright_black" if row["probability"] < propability_threshold else "white"
+        table.add_row(
+            row["purpose"][:50],
+            row["name"][:50],
+            f"{row['amount']:.2f}",
+            row["predicted_category"],
+            f"{row['probability']:.2f}",
+            style=probability_style
+        )
+    console.print(table)
 
     # Apply threshold and update transactions
     if apply:
+        df = df[df["probability"] >= propability_threshold]
         cursor = db.get_connection().cursor()
         for transaction_id, predicted_category_id, probabilities in df[["transaction_id", "predicted_category_id", "probability"]].values:
-            if probabilities > propability_threshold:
-                cursor.execute("UPDATE transactions SET category_key = ? WHERE rowid = ?", (int(predicted_category_id), int(transaction_id)))
+            cursor.execute("UPDATE transactions SET category_key = ? WHERE rowid = ?", (int(predicted_category_id), int(transaction_id)))
         db.get_connection().commit()
 
 
-class Word2VecVectorizer(BaseEstimator, TransformerMixin):
-    def __init__(self, model_name="glove-wiki-gigaword-50"):
-        self.model_name = model_name
-        self.word2vec = api.load(model_name)
-        self.dim = self.word2vec.vector_size
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X):
-        return np.array([self._get_sentence_vector(sentence) for sentence in X])
-
-    def _get_sentence_vector(self, sentence):
-        words = sentence.split()
-        word_vectors = [self.word2vec[word] for word in words if word in self.word2vec]
-        if len(word_vectors) == 0:
-            return np.zeros(self.dim)
-        return np.mean(word_vectors, axis=0)
-
-
-# Modify the train_model function
 @cli.command()
+# fmt: off
 @click.option("--db-password", help="Encryption password of moneymoney DB", required=True)
-@click.option(
-    "--date-from",
-    type=RelativeDate(),
-    default="2Y",
-    help="Oldest transaction to use for training (e.g., -1Y for one year ago or 2021-01-01 for an absolute date)",
-    required=True,
-    show_default=True,
-)
-@click.option(
-    "--date-to",
-    type=RelativeDate(),
-    default=datetime.datetime.now().strftime("%Y-%m-%d"),
-    help="Newest transaction to use for training (e.g., -3M for three months ago or 2021-01-01 for an absolute date)",
-    required=True,
-    show_default=True,
-)
+@click.option("--date-from", type=RelativeDate(), default="2Y", help="Oldest transaction to use for training (e.g., -1Y for one year ago or 2021-01-01 for an absolute date)", required=True, show_default=True)  # noqa: E501
+@click.option("--date-to", type=RelativeDate(), default=datetime.datetime.now().strftime("%Y-%m-%d"), help="Newest transaction to use for training (e.g., -3M for three months ago or 2021-01-01 for an absolute date)", required=True, show_default=True)  # noqa: E501
 @click.option("--limit-to-account", help="Limit training to transactions in the defined account. Can be provided multiple times", required=True, multiple=True)
 @click.option("--model-name", help="Specify the model name to be created", required=True, default="default", show_default=True)
-@click.option(
-    "--limit-to-category-file",
-    type=click.File(),
-    help="Provide a text file with a category ID per line to limit the training to those categories",
-    required=False,
-)
+@click.option("--limit-to-category-file", type=click.File(), help="Provide a text file with a category ID per line to limit the training to those categories", required=False)  # noqa: E501
+# fmt: on
 def train_model(db_password, date_from, date_to, limit_to_account, model_name, limit_to_category_file):
     db = MoneyMoneyDB(db_password)
 
